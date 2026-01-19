3775aece34de7df6f2d540c3579d47c0f14ab602
Your Name <your.email@example.com>
2026-01-19 22:06:21 +0530

    Adding the requirement.txt file

--- main.py
+++ main.py
@@ -1,20 +1,7 @@
 from fastapi import FastAPI
-import os
-import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "Hello from news-app!"}
-
-@app.get("/health")
-def health():
-    return {"status": "ok"}
-
-if __name__ == "__main__":
-    uvicorn.run(
-        app,
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 8000))
-    )
